1c8ab941bda3134b626e1808cd4071bfd0a1051f
lucklyYong <luckyzangvip@163.com>
2023-03-07 10:10:02 +0800

    下拉组件修复

--- src/components/SelectSearch.vue
+++ src/components/SelectSearch.vue
@@ -10,8 +10,8 @@
                 <van-icon name="search" size="20" />
 			</div>
 			<ul class="list-module">
-				<li v-for ="(item,index) in datalist" @click="clickItem(item)" :key="index" 
-				:class="{'liStyle': (item.value == currentFullValue['value']) && currentFullValue['value'],
+				<li v-for ="(item,index) in datalist" @click="clickItem(item,index)" :key="index" 
+				:class="{'liStyle': !multiple ? (item.value == currentFullValue['value']) && currentFullValue['value'] != null : item['selected'],
 					'liSelectItemStyle': item.text.indexOf('请选择')!=-1
 					}
 				">
@@ -24,6 +24,10 @@
 				</li>
 			</ul>
 			<div class="tip-nodata" v-show="searchValue.length && !datalist.length">暂无数据!</div>
+			<!-- <div class="operation-btn" v-if="multiple">
+				<span @click="isShow = false">取消</span>
+				<span @click="sureEvent">确定</span>
+			</div> -->
 		</div>
 	</div>
 </template>
@@ -37,11 +41,17 @@
 			current: '',
 			currentFullValue: null,
 			datalist:[],
+			selectedItem: [],
 			isShow:false
 		}
   },
   props:{
-		curData:String | Number | null,	// 当前选中数据
+	  	// 是否多选
+		multiple: {
+			type: Boolean,
+			default: false
+		},
+		curData: String | Number | null,	// 当前选中数据
 		itemData:Array,	// 所有选项数据
 		isNeedSearch:{	// 是否需要搜索
 			type: Boolean,
@@ -60,15 +70,47 @@
   watch: {
     curData: {
         handler: function(newVal, oldVal) {
-           this.current = this.datalist.length > 0 ? this.datalist.filter((item) => { return item.value == newVal})[0]['text'] : '';
-		   this.currentFullValue = this.datalist.length > 0 ? this.datalist.filter((item) => { return item.value == newVal})[0] : null
+		   //单选
+		   if (!this.multiple) {
+			 if (newVal == null || !isNaN(newVal)) {
+				this.current = this.datalist.length > 0 ? this.datalist.filter((item) => { return item.value == newVal})[0]['text'] : '';
+		   	 	this.currentFullValue = this.datalist.length > 0 ? this.datalist.filter((item) => { return item.value == newVal})[0] : null
+			 } else {
+				this.current = this.datalist.length > 0 ? this.datalist.filter((item) => { return item.value == newVal['value']})[0]['text'] : '';
+		   	 	this.currentFullValue = this.datalist.length > 0 ? this.datalist.filter((item) => { return item.value == newVal['value']})[0] : null
+			 }
+		   } else {
+			    if (newVal == null) {
+					this.current = this.datalist.length > 0 ? this.datalist.filter((item) => { return item.value == newVal})[0]['text'] : '';
+				} else {
+					let temporaryArray = [];
+					this.selectedItem = this.datalist.filter((innerItem) => { return innerItem['selected'] == true });
+					for (let it of this.datalist) {
+						if (it['selected']) {
+							temporaryArray.push(it['text'])
+						}
+					};
+					this.current = newVal.length > 0 ? temporaryArray.join(',') : this.itemData[0]['text']
+				}
+		   }
         },
         deep: true,
 		immediate: true
     },
 	itemData: {
         handler: function(newVal, oldVal) {
-           this.datalist = newVal
+           this.datalist = newVal;
+        },
+        deep: true,
+		immediate: true
+    },
+	selectedItem: {
+        handler: function(newVal, oldVal) {
+           if (this.multiple) {
+			   if (this.selectedItem.length == 0) {
+				   this.current = this.itemData[0]['text']
+			   }
+		   }
         },
         deep: true,
 		immediate: true
@@ -76,14 +118,37 @@
   },
   
   created(){
-	  console.log('值',this.curData,this.itemData);
 		this.datalist = this.itemData;
-        this.current = this.datalist.filter((item) => { return item.value == this.curData})[0]['text'];
-		this.currentFullValue = this.datalist.filter((item) => { return item.value == this.curData})[0];
+		//单选
+		if (!this.multiple) {
+			if (this.curData == null || !isNaN(this.curData )) {
+				this.current = this.datalist.length > 0 ? this.datalist.filter((item) => { return item.value == this.curData})[0]['text'] : '';
+				this.currentFullValue = this.datalist.length > 0 ? this.datalist.filter((item) => { return item.value == this.curData})[0] : null
+			} else {
+				this.current = this.datalist.length > 0 ? this.datalist.filter((item) => { return item.value == this.curData['value']})[0]['text'] : '';
+				this.currentFullValue = this.datalist.length > 0 ? this.datalist.filter((item) => { return item.value == this.curData['value']})[0] : null
+			}
+		} else {
+			if (this.curData == null) {
+					this.current = this.datalist.length > 0 ? this.datalist.filter((item) => { return item.value == this.curData})[0]['text'] : '';
+				} else {
+					let temporaryArray = [];
+					this.selectedItem = this.datalist.filter((innerItem) => { return innerItem['selected'] == true });
+					for (let it of this.itemData) {
+						if (it['selected']) {
+							temporaryArray.push(it['text'])
+						}
+					};
+					this.current = this.curData.length > 0 ? temporaryArray.join(',') : this.itemData[0]['text']
+				}
+		};
 		//点击组件以外的地方，收起
 		document.addEventListener('click', (e) => {
 			if (!this.$el.contains(e.target)){
-				this.isShow = false
+				this.isShow = false;
+				if (this.multiple) {
+					this.$emit('change',this.selectedItem)
+				}
 			}
 		}, false)
   },
@@ -92,30 +157,105 @@
             this.isShow = !this.isShow;
             if (this.isShow) {
                 this.searchValue = '';
-                this.datalist = this.itemData
-            }
+                this.datalist = this.itemData;
+				if (this.multiple) {
+					if (this.current != null && this.current != this.itemData[0]['text']) {
+						let temporaryArray = this.current.split(',');
+						this.datalist.forEach((item) => {
+							if (temporaryArray.indexOf(item.text) != -1) {
+								item.selected = true
+							}
+						})
+					} else {
+						this.datalist.forEach(element => {
+							if (element['value']) {
+								element['selected'] = false
+							}
+						})
+					}
+				}
+            } else {
+				if (this.multiple) {
+					this.$emit('change',this.selectedItem)
+				}
+			}
         },
 
 		search(e){
 			this.searchValue = e.target.value;
+			//单选
 			this.datalist = this.itemData.filter((item)=>{
 				return item.text.indexOf(this.searchValue) != -1
 			})
 		},
         
-		clickItem(item){
-			console.log('带年纪后',item);
-			this.current = this.datalist.filter((innerItem) => { return innerItem.value == item.value})[0]['text'];
-			this.currentFullValue = this.datalist.filter((innerItem) => { return innerItem.value == item.value})[0];
+		clickItem(item,index){
+			// 单选
+			if (!this.multiple) {
+				this.current = this.datalist.filter((innerItem) => { return innerItem.value == item.value})[0]['text'];
+				this.currentFullValue = this.datalist.filter((innerItem) => { return innerItem.value == item.value})[0];
+				this.isShow = false;
+				this.$emit('change',item)
+			} else {
+				if (item['value'] == null) { return };
+				this.datalist[index]['selected'] = !this.datalist[index]['selected'];
+				this.selectedItem = this.itemData.filter((innerItem) => { return innerItem['selected'] == true });
+				this.$emit('change',this.selectedItem);
+				let temporaryArray = [];
+				for (let it of this.selectedItem) {
+					temporaryArray.push(it['text'])
+				};
+				this.current = temporaryArray.join(',')
+			}
+		},
+
+		// 确定事件
+		sureEvent () {
+			this.$emit('change',this.selectedItem);
 			this.isShow = false;
-			this.$emit('change',item)
+			console.log('多选',)
 		},
 
 		//供父组件调用的清除选择框值的方法
 		clearSelectValue () {
-			this.current = this.datalist[0]['text'];
-			this.currentFullValue = this.datalist[0];
-			this.$emit('change',{text:this.datalist[0]['text'],value: null})
+			// 单选
+			if (!this.multiple) {
+				this.current = this.itemData[0]['text'];
+				this.currentFullValue = this.datalist[0];
+				this.$emit('change',{text:this.itemData[0]['text'],value: null})
+			} else {
+				this.datalist.forEach((element) => { 
+					if (element['value']) {
+						element['selected'] = false
+					}
+				});
+				this.selectedItem = this.datalist.filter((innerItem) => { return innerItem['selected'] == true });
+				this.$emit('change',this.selectedItem);
+				let temporaryArray = [];
+				for (let it of this.selectedItem) {
+					temporaryArray.push(it['text'])
+				};
+				this.current = temporaryArray.join(',')
+			};
+			this.datalist = this.itemData
+		},
+
+		//供父组件调用的全选的方法
+		selectAllValue () {
+			if (this.multiple) {
+				this.datalist.forEach(element => { 
+					if (element['value']) {
+						element['selected'] = true
+					}
+				});
+				this.selectedItem = this.datalist.filter((innerItem) => { return innerItem['selected'] == true });
+				this.$emit('change',this.selectedItem);
+				let temporaryArray = [];
+				for (let it of this.selectedItem) {
+					temporaryArray.push(it['text'])
+				};
+				this.current = temporaryArray.join(',')
+			}
 		}
 	}
 }
@@ -164,20 +304,20 @@
 		}
 		.list-module {
 		 max-height: 150px;
-		 overflow-y: auto;
+		 overflow: scroll;
 			li {
                 word-break: break-all;
                 line-height: 20px;
-				&._self-hide {
-					display: none;
-				}
-				padding: 5px 8px;
-				box-sizing: border-box;
-				&:hover {
-					cursor:pointer;
-					color: #fff;
-					background: #00a0e9;
-				};
+				// &._self-hide {
+				// 	display: none;
+				// }
+				// padding: 5px 8px;
+				// box-sizing: border-box;
+				// &:hover {
+				// 	cursor:pointer;
+				// 	color: #fff;
+				// 	background: #00a0e9;
+				// };
 				>span {
 					&:nth-child(1) {
 						margin-right: 10px
@@ -217,6 +357,34 @@
 		width: 100%;
 		&.on{
 			display: block
+		};
+		.operation-btn {
+		  height: 60px;
+          display: flex;
+          width: 90%;
+          margin: 0 auto;
+          align-items: center;
+          justify-content: center;
+          >span {
+              width: 40%;
+              display: inline-block;
+              height: 35px;
+              font-size: 14px;
+              line-height: 35px;
+              background: #fff;
+              text-align: center;
+              border-radius: 6px;
+              &:nth-child(1) {
+                  color: #ccc;
+                  margin-right: 40px;
+				  box-sizing: border-box;
+				  border: 1px solid #ccc
+              };
+              &:last-child {
+                  color: #fff;
+				  background: #2390fe
+              }
+          }
 		}
 	}
 	.cur-name{
